c26796a2318b232002df07d080e212ecfdfafabb
ConorFayleTCO <141320269+ConorFayleTCO@users.noreply.github.com>
2023-09-22 13:11:40 +0100

    TD-5: applicant submits application (#3)
    * TD-5|begin adding test for applicant submitting application
    
    * TD-5|applicant logs in to dashboard
    
    * TD-5|APPLICANT CAN SUBMIT APPLICATION WOO
    
    * TD-5|add mochawesome html report generator
    
    * TD-5|add charts to report
    
    * TD-5|add tech debt ticket as comment
    
    * TD-5|update readme
    
    * TD-5|improve readme, make changes according to PR using cy:open:watch and cy:open:nowatch
    
    * TD-5|little refactor to group repeated actions
    
    ---------
    
    Co-authored-by: conor <conor.fayle@and.digital>

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from "cypress";
+require("dotenv").config();
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+    env: {
+      oneLoginSandboxUsername: process.env["one-login-sandbox-username"],
+      oneLoginSandboxPassword: process.env["one-login-sandbox-password"],
+      oneLoginApplicantEmail: process.env["one-login-applicant-email"],
+      oneLoginApplicantPassword: process.env["one-login-applicant-password"],
+      oneLoginAdminEmail: process.env["one-login-admin-email"],
+      oneLoginAdminPassword: process.env["one-login-admin-password"],
+      oneLoginSuperAdminEmail: process.env["one-login-super-admin-email"],
+      oneLoginSuperAdminPassword: process.env["one-login-super-admin-password"],
+    },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "mochawesome-report",
+      charts: true,
+      reportPageTitle: "Find a Grant",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+      debug: true,
     },
   },
 });
