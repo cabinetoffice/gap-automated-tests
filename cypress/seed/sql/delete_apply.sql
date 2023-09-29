738bb7c5cfa5f0d24d6e81e17b2fc9aedc2a1455
ConorFayleTCO <141320269+ConorFayleTCO@users.noreply.github.com>
2023-09-29 16:51:01 +0100

    Update delete_apply.sql

--- cypress/seed/sql/delete_apply.sql
+++ cypress/seed/sql/delete_apply.sql
@@ -1,12 +1,52 @@
-delete from public.grant_advert where grant_advert_id in ('00000000-0000-0000-0000-000000000000') or created_by in ('-2');
+delete from public.grant_advert where grant_advert_id in ('00000000-0000-0000-0000-000000000000') or created_by in (
+    select grant_admin_id from public.grant_admin where grant_admin_id in ('-1', '-2') OR user_id in (
+        SELECT gap_user_id FROM gap_user WHERE user_sub in (
+            'urn:fdc:gov.uk:2022:fWal-gUz2TTpPZrp9-PgbW5jOk10hXfTy3cgxxW5HRE',
+            'urn:fdc:gov.uk:2022:1QUViebEw1PHGV7iShahw7hmaSG9OL4RFzY_WgR2qcY',
+            'urn:fdc:gov.uk:2022:HvA7mHQsM_eNX5tAYNj2Oyj8_d3sooEtRjo7wbOaROY'
+        )
+    )
+);
 
-delete from public.grant_submission where application_id in ('-1');
+delete from public.grant_submission where application_id in ('-1') OR scheme_id in (
+    select grant_scheme_id from public.grant_scheme where grant_scheme_id in ('-1') or created_by in (
+        select grant_admin_id from public.grant_admin where grant_admin_id in ('-1', '-2') OR user_id in (
+            SELECT gap_user_id FROM gap_user WHERE user_sub in (
+                'urn:fdc:gov.uk:2022:fWal-gUz2TTpPZrp9-PgbW5jOk10hXfTy3cgxxW5HRE',
+                'urn:fdc:gov.uk:2022:1QUViebEw1PHGV7iShahw7hmaSG9OL4RFzY_WgR2qcY',
+                'urn:fdc:gov.uk:2022:HvA7mHQsM_eNX5tAYNj2Oyj8_d3sooEtRjo7wbOaROY'
+            )
+        )
+    )
+);
 
-delete from public.grant_application where grant_application_id in ('-1') or created_by in ('-2');
+delete from public.grant_application where grant_application_id in ('-1') or created_by in (
+    select grant_admin_id from public.grant_admin where grant_admin_id in ('-1', '-2') OR user_id in (
+        SELECT gap_user_id FROM gap_user WHERE user_sub in (
+            'urn:fdc:gov.uk:2022:fWal-gUz2TTpPZrp9-PgbW5jOk10hXfTy3cgxxW5HRE',
+            'urn:fdc:gov.uk:2022:1QUViebEw1PHGV7iShahw7hmaSG9OL4RFzY_WgR2qcY',
+            'urn:fdc:gov.uk:2022:HvA7mHQsM_eNX5tAYNj2Oyj8_d3sooEtRjo7wbOaROY'
+        )
+    )
+);
 
-delete from public.grant_scheme where grant_scheme_id in ('-1') or created_by in ('-2');
+delete from public.grant_scheme where grant_scheme_id in ('-1') or created_by in (
+    select grant_admin_id from public.grant_admin where grant_admin_id in ('-1', '-2') OR user_id in (
+        SELECT gap_user_id FROM gap_user WHERE user_sub in (
+            'urn:fdc:gov.uk:2022:fWal-gUz2TTpPZrp9-PgbW5jOk10hXfTy3cgxxW5HRE',
+            'urn:fdc:gov.uk:2022:1QUViebEw1PHGV7iShahw7hmaSG9OL4RFzY_WgR2qcY',
+            'urn:fdc:gov.uk:2022:HvA7mHQsM_eNX5tAYNj2Oyj8_d3sooEtRjo7wbOaROY'
+        )
+    )
+);
 
-delete from public.grant_admin where grant_admin_id in ('-1', '-2');
+delete from public.grant_admin where grant_admin_id in ('-1', '-2') OR user_id in (
+    SELECT gap_user_id FROM gap_user WHERE user_sub in (
+        'urn:fdc:gov.uk:2022:fWal-gUz2TTpPZrp9-PgbW5jOk10hXfTy3cgxxW5HRE',
+        'urn:fdc:gov.uk:2022:1QUViebEw1PHGV7iShahw7hmaSG9OL4RFzY_WgR2qcY',
+        'urn:fdc:gov.uk:2022:HvA7mHQsM_eNX5tAYNj2Oyj8_d3sooEtRjo7wbOaROY'
+    )
+);
 
 delete from public.grant_applicant where
     id in ('-1', '-2', '-3') OR
