5ecebca09fc5eef41927e694530d1f57a976914c
conor <conor.fayle@and.digital>
2023-11-09 14:57:26 +0000

    GAP-2274 | clean up console logs

--- cypress/seed/database.ts
+++ cypress/seed/database.ts
@@ -2,27 +2,6 @@ import { Client } from "pg";
 import { promises as fs } from "fs";
 import "dotenv/config";
 
-export const runSQL = async (
-  filePath: string,
-  dbName: string,
-  dbUrl: string,
-): Promise<void> => {
-  try {
-    const connectionString: string = getConnectionStringByDbName(dbUrl, dbName);
-    // console.log("Connection:" + connectionString);
-    const sqlScript: string = await fs.readFile(filePath, "utf8");
-    const client = new Client({ connectionString });
-    await client.connect();
-    // console.log("sqlScript: ", sqlScript);
-    await client.query(sqlScript);
-    await client.end();
-
-    // console.log("SQL script executed successfully.");
-  } catch (error) {
-    console.error("Error executing SQL script: ", error);
-  }
-};
-
 export const runSQLFromJs = async (
   sqlScripts: Array<string>,
   substitutions: any,
@@ -31,16 +10,12 @@ export const runSQLFromJs = async (
 ): Promise<void> => {
   try {
     const connectionString: string = getConnectionStringByDbName(dbUrl, dbName);
-    // console.log("Connection:" + connectionString);
     const client = new Client({ connectionString });
     await client.connect();
-    // console.log("sqlScript: ", sqlScript);
     for (const sqlScript of sqlScripts) {
       await client.query(sqlScript, substitutions[sqlScript] || []);
     }
     await client.end();
-
-    // console.log("SQL script executed successfully.");
   } catch (error) {
     console.error("Error executing SQL script: ", error);
   }
