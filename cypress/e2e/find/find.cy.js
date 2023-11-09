84c3571424edbafb5f8805b2e50d1fb5975a062d
conor <conor.fayle@and.digital>
2023-11-09 11:23:06 +0000

    GAP-2274 | fix find test

--- cypress/e2e/find/find.cy.js
+++ cypress/e2e/find/find.cy.js
@@ -87,15 +87,15 @@ describe("Find a Grant", () => {
     const grantData = {
       Location: "National",
       "Funding organisation": "The Department of Business",
-      "Who can apply": "Non-profit",
+      "Who can apply": "Personal / Individual",
       "How much you can get": "From £1 to £10,000",
       "Total size of grant scheme": "£1 million",
       "Opening date": "24 August 2023, 12:01am",
       "Closing date": "24 October 2040, 11:59pm",
     };
     Object.entries(grantData).forEach(([key, value]) => {
-      cy.contains(key);
-      cy.contains(value);
+      cy.get("#cypress_test_advert_contentful_slug").contains(key);
+      cy.get("#cypress_test_advert_contentful_slug").contains(value);
     });
   });
 
