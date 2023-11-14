0774d8fef7136919985025fbfcbf11fea6e2a38a
Conor Fayle <141320269+ConorFayleTCO@users.noreply.github.com>
2023-11-14 10:57:26 +0000

    GAP-2286: Simultaneous E2E runs (#33)
    * GAP-2286 | parameterise user ids to avoid data collisions. temp remove specific inserts and deletions pending if-not-exists functionality
    
    * GAP-2286 | fix env
    
    * GAP-2286|modify scripts and substitutions
    
    * GAP-2286 | add script substitutions and refactor
    
    * GAP-2286|remove it.only
    
    * GAP-2286 | get concurrent runs working wahoo
    
    * GAP-2286 | negate absolute value of FIRST_USER_ID
    
    * GAP-2286 | refactor
    
    ---------
    
    Co-authored-by: conor <conor.fayle@and.digital>

--- cypress/common/constants.ts
+++ cypress/common/constants.ts
@@ -1,1 +1,10 @@
-export const TEST_GRANT_NAME = "Cypress - Automated E2E Test Grant V1";
+import "dotenv/config";
+
+const firstUserId = Math.abs(+process.env.FIRST_USER_ID);
+export const TEST_V1_GRANT = {
+  name: `Cypress - Automated E2E Test Grant V1 ID:${firstUserId}`,
+  contentfulId: `cypress_test_advert_contentful_id_${firstUserId}`,
+  contentfulSlug: `cypress_test_advert_contentful_slug_${firstUserId}`,
+  applicationUrl: `${process.env.POST_LOGIN_BASE_URL}/apply/applicant/applications/-${firstUserId}`,
+  applicationName: "Cypress - Test Application V1",
+};
