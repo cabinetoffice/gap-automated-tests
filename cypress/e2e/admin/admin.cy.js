43788a74ded77a27f0d810f7e961f6d539f5bbb1
john-tco <135222889+john-tco@users.noreply.github.com>
2023-12-13 16:49:40 +0000

    GAP-2348: Admin Dashboard tests  (#53)
    * initial
    
    * add logs
    
    * rm
    
    * feedback

--- cypress/e2e/admin/admin.cy.js
+++ cypress/e2e/admin/admin.cy.js
@@ -8,6 +8,9 @@ import {
   clickBack,
   clickSaveAndContinue,
   assert200,
+  BASE_URL,
+  assert404,
+  signInAsSuperAdmin,
 } from "../../common/common";
 import {
   publishAdvert,
@@ -46,11 +49,72 @@ const {
 const { SENT, SEND_ERROR, GGIS_ERROR, VALIDATION_ERROR } =
   SPOTLIGHT_SUBMISSION_STATUS;
 
+const ADMIN_DASHBOARD_URL = `${BASE_URL}/apply/admin/dashboard`;
+const SUPER_ADMIN_DASHBOARD_URL = `${BASE_URL}/apply/admin/super-admin-dashboard`;
+
 describe("Create a Grant", () => {
   beforeEach(() => {
     cy.task("setUpUser");
     cy.task("setUpApplyData");
     signInToIntegrationSite();
+  });
+
+  it("Applicant cannot view the admin dashboard", () => {
+    cy.get("[data-cy=cySignInAndApply-Link]").click();
+    signInAsApplyApplicant();
+    cy.log("asserting that the applicant cannot view the admin dashboard");
+    assert404(ADMIN_DASHBOARD_URL);
+  });
+
+  it("Admin can view the dashboard, cannot access the super-admin dashboard", () => {
+    cy.get("[data-cy=cySignInAndApply-Link]").click();
+    signInAsAdmin();
+
+    cy.log("Logged into admin account - asserting on dashboard content");
+    cy.contains("Your grants");
+    cy.contains(Cypress.env("testV1InternalGrant").schemeName);
+    cy.contains(Cypress.env("testV2InternalGrant").schemeName);
+    cy.contains("View all grants").click();
+    cy.contains("All grants");
+    cy.log("asserting on content within the 'all grants' page");
+    cy.contains(Cypress.env("testV2ExternalGrant").schemeName);
+    cy.contains(Cypress.env("testV1ExternalGrant").schemeName);
+    cy.contains(Cypress.env("testV2InternalGrant").schemeName);
+    cy.contains(Cypress.env("testV1InternalGrant").schemeName);
+    clickText("View");
+    cy.contains("Grant summary");
+    cy.log("asserting the admin cannot view the super admin dashboard");
+    assert404(SUPER_ADMIN_DASHBOARD_URL);
+  });
+
+  it("Applicant promoted to admin can view the dashboard", () => {
+    cy.get("[data-cy=cySignInAndApply-Link]").click();
+    signInAsSuperAdmin();
+    cy.log("Promoting applicant account -> admin account");
+    cy.get("[name=searchTerm]").type(Cypress.env("oneLoginApplicantEmail"));
+    cy.get("[data-cy=cy-button-Search]").click();
+    cy.contains("Edit").first().click();
+    clickText("Change");
+    cy.get("[data-cy=cy-checkbox-value-3]").check();
+    cy.log("Changing user roles");
+    clickText("Change Roles");
+    cy.log("Adding a department");
+    cy.contains("Change").first().click();
+    clickText("Cypress - Test Department");
+    clickText("Change department");
+    signOut();
+    cy.log(
+      "Super admin signed out -> logging in with newly promoted admin account",
+    );
+    cy.get("[data-cy=cySignInAndApply-Link]").click();
+    signInAsApplyApplicant();
+    cy.log("asserting on content within the admin dashboard");
+    cy.contains("Manage a grant");
+    cy.contains(
+      "Use this service to add your grant details and build an application form for applicants to use.",
+    );
+    cy.contains("Add grant details");
+    cy.contains("Start by adding the details of your grant.");
   });
 
   it("can create a new Grant and create advert", () => {
