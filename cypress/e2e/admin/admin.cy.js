21bbb0d2b671d3e838e59cddaec98d0a7f3990ef
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2024-01-24 12:30:14 +0000

    PR feedback

--- cypress/e2e/admin/admin.cy.js
+++ cypress/e2e/admin/admin.cy.js
@@ -1,4 +1,5 @@
 import {
+  BASE_URL,
   SUPER_ADMIN_DASHBOARD_URL,
   assert200,
   assert404,
@@ -6,9 +7,9 @@ import {
   clickSaveAndContinue,
   clickText,
   downloadFileFromLink,
-  searchForGrant,
   log,
   saveAndExit,
+  searchForGrant,
   signInAsAdmin,
   signInAsApplyApplicant,
   signInAsSuperAdmin,
@@ -25,12 +26,12 @@ import {
   advertSection5,
   applicationForm,
   convertDateToString,
-  validateSubmissionDownload,
   createGrant,
   publishAdvert,
   publishApplication,
   publishApplicationForm,
   searchForAGrant,
+  validateSubmissionDownload,
 } from "./helper";
 
 import {
@@ -40,6 +41,8 @@ import {
   fillOutRequiredChecks,
   submitApplication,
 } from "../applicant/helper";
+
+const API_DASHBOARD_BASE_URL = BASE_URL + "/find/api/admin";
 
 const {
   UPDATE_SPOTLIGHT_SUBMISSION_STATUS,
@@ -779,5 +782,92 @@ describe("Downloads and Due Diligence", () => {
     );
     validateSubmissionDownload(Cypress.env("testV1InternalGrant").schemeId, 2);
     cy.readFile("cypress/downloads/unzip/submission_export/example_1.doc");
+  });
+});
+
+describe("API dashboard", () => {
+  beforeEach(() => {
+    signInToIntegrationSite();
+  });
+
+  it("Admin users should not have access to any API dashboard endpoints", () => {
+    cy.log("Clicking Sign in as an Admin");
+    cy.get("[data-cy=cySignInAndApply-Link]").click();
+
+    signInAsAdmin();
+
+    cy.getCookie("user-service-token").then((cookie) => {
+      cy.log("sending a POST request to /api-keys/create");
+      cy.request(
+        {
+          failOnStatusCode: false,
+          method: "POST",
+          url: `${API_DASHBOARD_BASE_URL}/api-keys/create`,
+          headers: {
+            Cookie: cookie.value,
+          },
+        },
+        {
+          keyName: "Cypress",
+        },
+      ).then((r) => {
+        expect(r.status).to.eq(500);
+        expect(r.redirects[0]).to.contain(`/find/api/admin/error`);
+      });
+
+      cy.log("sending a GET request to /api-keys/create");
+      cy.request(
+        {
+          failOnStatusCode: false,
+          method: "GET",
+          url: `${API_DASHBOARD_BASE_URL}/api-keys/create`,
+          headers: {
+            Cookie: cookie.value,
+          },
+        },
+        {
+          keyName: "Cypress",
+        },
+      ).then((r) => {
+        expect(r.status).to.eq(500);
+        expect(r.redirects[0]).to.contain(`/find/api/admin/error`);
+      });
+
+      cy.log("sending a GET request to /api-keys");
+      cy.request(
+        {
+          failOnStatusCode: false,
+          method: "GET",
+          url: `${API_DASHBOARD_BASE_URL}/api-keys`,
+          headers: {
+            Cookie: cookie.value,
+          },
+        },
+        {
+          keyName: "Cypress",
+        },
+      ).then((r) => {
+        expect(r.status).to.eq(500);
+        expect(r.redirects[0]).to.contain(`/find/api/admin/error`);
+      });
+
+      cy.log("sending a GET request to /api-keys/manage");
+      cy.request(
+        {
+          failOnStatusCode: false,
+          method: "GET",
+          url: `${API_DASHBOARD_BASE_URL}/api-keys/manage`,
+          headers: {
+            Cookie: cookie.value,
+          },
+        },
+        {
+          keyName: "Cypress",
+        },
+      ).then((r) => {
+        expect(r.status).to.eq(500);
+        expect(r.redirects[0]).to.contain(`/find/api/admin/error`);
+      });
+    });
   });
 });
