abcb5bbf73d7b2b24ff9f42cb6cf0f95b333a99e
conor <conor.fayle@and.digital>
2024-01-25 11:57:38 +0000

    GAP-2400 | add teardown

--- cypress/seed/ts/selectApplyData.ts
+++ cypress/seed/ts/selectApplyData.ts
@@ -9,7 +9,7 @@ SELECT * FROM public.grant_export WHERE submission_id IN (
     SELECT id FROM public.grant_submission WHERE applicant_id IN ($1, $2, $3) OR scheme_id IN (
         SELECT grant_scheme_id from public.grant_scheme WHERE created_by IN (
             SELECT grant_admin_id from public.grant_admin WHERE grant_admin_id IN ($4, $5) OR user_id IN (
-                SELECT gap_user_id FROM gap_user WHERE user_sub IN ($6, $7, $8)
+                SELECT gap_user_id FROM gap_user WHERE user_sub IN ($6, $7, $8, $9)
             )
         )
     )
