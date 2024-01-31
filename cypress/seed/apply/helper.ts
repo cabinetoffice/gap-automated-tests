2c055d02fbe0daff3f81f116913291fbfea17cc4
Conor Fayle <141320269+ConorFayleTCO@users.noreply.github.com>
2024-01-31 14:15:29 +0000

    add retry timer (#92)
    Co-authored-by: conor <conor.fayle@and.digital>

--- cypress/seed/apply/helper.ts
+++ cypress/seed/apply/helper.ts
@@ -6,4 +6,34 @@ const getUUID = (index = 0) =>
 const getTestID = (index = 0) =>
   -(Math.abs(+process.env.FIRST_USER_ID) + index);
 
-export { getUUID, getTestID };
+function sleep(milliseconds: number) {
+  const start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
+
+const retry = async (
+  func: any,
+  condition: { (response: { status: string }): boolean; (arg0: any): any },
+  maxAttempts = 30,
+  delay = 1000,
+) => {
+  let i = 0;
+  for (i; i < maxAttempts; i++) {
+    console.log("attempt", i, " at running script");
+    const response = await func();
+    console.log(response[0][0]);
+    if (condition(response)) return response;
+    if (i < maxAttempts - 1) sleep(delay);
+  }
+  console.log(
+    "Could not get good response from script in ",
+    +maxAttempts + " number of attempts",
+  );
+  return null;
+};
+
+export { getUUID, getTestID, retry };
