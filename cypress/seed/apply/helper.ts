fca4ad9490d26a1e8faae15f3189af34710d8422
Conor Fayle <141320269+ConorFayleTCO@users.noreply.github.com>
2024-02-12 17:28:35 +0000

    fix flaky getExportedUrlAndLocation for QA (#107)
    * fix flaky getExportedUrlAndLocation for QA
    
    * refactor as per PR comments
    
    ---------
    
    Co-authored-by: conor <conor.fayle@and.digital>

--- cypress/seed/apply/helper.ts
+++ cypress/seed/apply/helper.ts
@@ -6,4 +6,28 @@ const getUUID = (index = 0) =>
 const getTestID = (index = 0) =>
   -(Math.abs(+process.env.FIRST_USER_ID) + index);
 
-export { getUUID, getTestID };
+const sleep = async (ms: number) =>
+  await new Promise((resolve) => setTimeout(resolve, ms));
+
+const retry = async (
+  func: any,
+  condition: { (response: { status: string }): boolean; (arg0: any): any },
+  maxAttempts = 30,
+  delay = 1000,
+) => {
+  let i = 0;
+  for (i; i < maxAttempts; i++) {
+    console.log("attempt", i, " at running script");
+    const response = await func();
+    console.log(response[0][0]);
+    if (condition(response)) return response;
+    if (i < maxAttempts - 1) sleep(delay);
+  }
+  console.log(
+    "Could not get good response from script in ",
+    +maxAttempts + " number of attempts",
+  );
+  return null;
+};
+
+export { getUUID, getTestID, retry };
