32da388c54feb750601dadb188a5b5a139bf2534
Conor Fayle <141320269+ConorFayleTCO@users.noreply.github.com>
2024-03-15 10:07:16 +0000

    GAP-2476: Co-authorship - Multiple editors (#166)
    * GAP-2476 | initiate co-auth test
    
    * GAP-2476 | add manage editors test
    
    * GAP-2476 | improve manage editors test
    
    * GAP-2476 | add tests for multiple editors using db version incrementing
    
    * GAP-2476 | remove hardcoded wait
    
    ---------
    
    Co-authored-by: conor <conor.fayle@and.digital>

--- cypress/seed/apply/service.ts
+++ cypress/seed/apply/service.ts
@@ -55,6 +55,7 @@ import {
   readdQueuedSpotlightSubmissions,
   removeQueuedSpotlightSubmissions,
   updateSpotlightSubmissionStatus,
+  incrementApplicationFormVersion,
 } from '../ts/updateApplyData';
 import {
   ADMIN_ID,
@@ -79,6 +80,7 @@ import {
 } from './constants';
 
 import { retry } from '../helper';
+import { getTestID } from './helper';
 
 const FIRST_USER_ID = process.env.FIRST_USER_ID;
 const AWS_ENVIRONMENT = process.env.AWS_ENVIRONMENT;
@@ -146,6 +148,14 @@ const deleteApplySchemes = async (): Promise<void> => {
     applyDeleteSubstitutions,
   );
   console.log('Successfully deleted schemes');
+};
+
+const simulateMultipleApplicationFormEditors = async (): Promise<void> => {
+  const V1_APPLICATION_ID = getTestID();
+  console.log('Bumping application form version for ID:', V1_APPLICATION_ID);
+  await runSqlForApply([incrementApplicationFormVersion], {
+    [incrementApplicationFormVersion]: [V1_APPLICATION_ID],
+  });
 };
 
 const createApiKeysData = async (): Promise<void> => {
@@ -441,5 +451,6 @@ export {
   updateSpotlightSubmission,
   insertSubmissionAndExport,
   addAdminInTechSupportTable,
+  simulateMultipleApplicationFormEditors,
   type ApiKeyDb,
 };
